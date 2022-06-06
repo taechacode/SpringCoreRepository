e547260d2ce8a7489cdb2b9b87b1ac704ac4d172
taechacode <kangtaechan2015@gmail.com>
2022-06-06 23:43:29 +0900

    Create hello.core package 16강 주문과 할인 도메인 실행과 테스트까지의 예제 코드 작성

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    
+    private int discountFixAmount = 1000; // 1000원 할인
+    
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) { // VIP면 1000원 고정 할인
+            return discountFixAmount;
+        } else { // 만약 VIP가 아니면 할인 없음(0)
+            return 0;
+        }
+    }
+    
+}
